1de07fba3c37d9c3ae28748f5c3e3e3b00fba07e
rileypk8 <rileypk8@gmail.com>
2024-08-29 23:30:22 -0400

    A5 - nodey

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import express from 'express'
-import cors from "cors";
+import CourseRoutes from "./Kanbas/Courses/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
+
 import Lab5 from "./Lab5/index.js";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+ModuleRoutes(app);
+CourseRoutes(app);
 Lab5(app);
 app.listen(4000)
