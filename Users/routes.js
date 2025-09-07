bcbb0ac919c2485c55e1670e21fea0510eb80cd2
rileypk8 <rileypk8@gmail.com>
2025-09-06 21:09:10 -0400

    a6: end of section 3

--- Users/routes.js
+++ Users/routes.js
@@ -1,8 +1,14 @@
 import * as dao from "./dao.js";
 let currentUser = null;
 export default function UserRoutes(app) {
-    const createUser = async (req, res) => { };
-    const deleteUser = async (req, res) => { };
+    const createUser = async (req, res) => {
+        const user = await dao.createUser(req.body);
+        res.json(user);
+    };
+    const deleteUser = async (req, res) => {
+        const status = await dao.deleteUser(req.params.userId);
+        res.json(status);
+    };
     const findAllUsers = async (req, res) => {
         const { role, name } = req.query;
         if (role) {
@@ -22,7 +28,11 @@ export default function UserRoutes(app) {
         const user = await dao.findUserById(req.params.userId);
         res.json(user);
     };
-    const updateUser = async (req, res) => { };
+    const updateUser = async (req, res) => {
+        const { userId } = req.params;
+        const status = await dao.updateUser(userId, req.body);
+        res.json(status);
+    };
     const signup = async (req, res) => { };
     const signin = async (req, res) => { };
     const signout = (req, res) => { };
